e873b05da444f983b8fc775287d78ef3be0251a0
BuddyActivist <buddyactivist@gmail.com>
2026-01-18 16:15:16 +0100

    Enhance global map rendering with search bar

--- includes/class-bagl-map.php
+++ includes/class-bagl-map.php
@@ -15,7 +15,7 @@ class BAGL_Map {
     }
 
     /**
-     * Render the global map container and enqueue JS
+     * Render the global map container + search bar + enqueue JS
      */
     public static function render_global_map( $atts ) {
 
@@ -28,12 +28,39 @@ class BAGL_Map {
             true
         );
 
-        // Pass REST URL to JS
+        // Load JS for search
+        wp_enqueue_script(
+            'bagl-group-global-search',
+            BAGL_URL . 'assets/js/group-global-search.js',
+            array( 'bagl-group-global-map' ),
+            '1.0.0',
+            true
+        );
+
+        // Load CSS
+        wp_enqueue_style(
+            'bagl-map-css',
+            BAGL_URL . 'assets/css/map.css',
+            array(),
+            '1.0.0'
+        );
+
+        // Pass REST URL + map defaults
         wp_localize_script( 'bagl-group-global-map', 'BAGL_MAP', array(
             'rest_url' => esc_url_raw( rest_url( 'bagl/v1/groups' ) ),
+            'center'   => array(
+                'lat'  => (float) get_option( 'bagl_map_center_lat', 41.9 ),
+                'lng'  => (float) get_option( 'bagl_map_center_lng', 12.5 ),
+                'zoom' => (int)   get_option( 'bagl_map_zoom', 6 ),
+            ),
         ) );
 
-        // Map container
-        return '<div id="bagl-group-global-map" style="width:100%;height:600px;"></div>';
+        // Search bar + map container
+        return '
+<div class="bagl-global-search-wrapper">
+    <input type="text" id="bagl-global-search" placeholder="' . esc_attr__( 'Search groups or places...', 'buddyactivist-group-location' ) . '">
+</div>
+<div id="bagl-group-global-map"></div>
+';
     }
 }
